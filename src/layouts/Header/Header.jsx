0a75b93a47067e2c2cabd7d4d3a775e1eb77e238
nursultan <znslt24@gmail.com>
2025-05-09 16:53:38 +0300

    Implement overlay menu, HTML dialog element, and display contents

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,73 +1,70 @@
-import './Header.scss'
-import Logo from '@/components/Logo'
-import classNames from 'classnames'
-import Button from '@/components/Button'
-import BurgerButton from '@/components/BurgerButton'
+import "./Header.scss"
+import Logo from "@/components/Logo"
+import classNames from "classnames"
+import Button from "@/components/Button"
+import BurgerButton from "@/components/BurgerButton"
 
 const Header = (props) => {
-  const {
-    url,
-  } = props
+  const { url } = props
 
   const menuItems = [
     {
-      label: 'Home',
-      href: '/',
+      label: "Home",
+      href: "/",
     },
     {
-      label: 'Movies & Shows',
-      href: '/movies',
+      label: "Movies & Shows",
+      href: "/movies",
     },
     {
-      label: 'Support',
-      href: '/support',
+      label: "Support",
+      href: "/support",
     },
     {
-      label: 'Subscriptions',
-      href: '/subscriptions',
+      label: "Subscriptions",
+      href: "/subscriptions",
     },
   ]
 
   return (
     <header className="header">
       <div className="header__inner container">
-        <Logo
-          className="header__logo"
-          loading="eager"
-        />
-        <nav className="header__menu">
-          <ul className="header__menu-list">
-            {menuItems.map(({ label, href }, index) => (
-              <li className="header__menu-item" key={index}>
-                <a
-                  className={classNames('header__menu-link', {
-                    'is-active': href === url
-                  })}
-                  href={href}
-                >
-                  {label}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="header__actions">
-          <Button
-            className="header__button"
-            label="Search"
-            isLabelHidden
-            mode="transparent"
-            iconName="search"
-          />
-          <Button
-            className="header__button"
-            label="Notifications"
-            isLabelHidden
-            mode="transparent"
-            iconName="notification"
-          />
-        </div>
-        <BurgerButton className="header__burger-button" />
+        <Logo className="header__logo" loading="eager" />
+        <dialog className="header__overlay-menu-dialog">
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {menuItems.map(({ label, href }, index) => (
+                <li className="header__menu-item" key={index}>
+                  <a
+                    className={classNames("header__menu-link", {
+                      "is-active": href === url,
+                    })}
+                    href={href}
+                  >
+                    {label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="header__actions">
+            <Button
+              className="header__button"
+              label="Search"
+              isLabelHidden
+              mode="transparent"
+              iconName="search"
+            />
+            <Button
+              className="header__button"
+              label="Notifications"
+              isLabelHidden
+              mode="transparent"
+              iconName="notification"
+            />
+          </div>
+        </dialog>
+        <BurgerButton className="header__burger-button visible-tablet" />
       </div>
     </header>
   )
