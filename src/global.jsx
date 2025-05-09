b475da29576412a751694a2e0ea8e3ddfe5f711b
nursultan <znslt24@gmail.com>
2025-05-09 14:07:47 +0300

    Implement navigation menu and lists in JSX

--- src/global.jsx
+++ src/global.jsx
@@ -5,7 +5,7 @@ import Footer from "@/layouts/Footer"
 import { Head } from "minista"
 
 export default function (props) {
-  const { children, title } = props
+  const { children, title, url } = props
 
   return (
     <>
@@ -31,7 +31,7 @@ export default function (props) {
         />
         <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
-      <Header />
+      <Header url={url} />
       <Content>{children}</Content>
       <Footer />
     </>
