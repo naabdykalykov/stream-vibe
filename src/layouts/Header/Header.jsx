34b242765840c46a4f73e63cbf3fe17084b7a86f
nursultan <znslt24@gmail.com>
2025-05-09 13:38:59 +0300

    Add sticky effect, scroll animation-timeline, Logo component, and classNames library

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,5 +1,14 @@
+import "./Header.scss"
+import Logo from "@/components/Logo"
+
 const Header = () => {
-  return <header>HEADER</header>
+  return (
+    <header className="header">
+      <div className="header__inner container">
+        <Logo loading="eager" />
+      </div>
+    </header>
+  )
 }
 
 export default Header
